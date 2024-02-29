11370b0f36f6d1a03359b938ea09fc02c566289b
ssssota <tommysouta@gmail.com>
2024-02-29 22:11:57 +0900

    feat: add tokenize function

--- packages/tokenizer/mod.ts
+++ packages/tokenizer/mod.ts
@@ -1,2 +1,13 @@
-export * from "./token.ts";
-export * from "./tokenizer.ts";
+import type { Token } from "./token.ts";
+import { Tokenizer } from "./tokenizer.ts";
+
+/**
+ * Tokenize Fastly VCL source code
+ * @param input Fastly VCL source code
+ * @returns tokens
+ */
+export function tokenize(input: string): Token[] {
+  return Array.from(new Tokenizer(input));
+}
+
+export { type Token, Tokenizer };
