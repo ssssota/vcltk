aefe0a6400d74776ad7a4767ae91c374ce6ec39a
ssssota <tommysouta@gmail.com>
2024-04-11 21:06:06 +0900

    chore: bumpup

--- packages/parser/deps.ts
+++ packages/parser/deps.ts
@@ -1,4 +1,3 @@
-// export type * from "jsr:@vcltk/ast@0.1.1";
+export type * from "jsr:@vcltk/ast@0.1.2";
 export type * from "jsr:@vcltk/token@0.2.0";
 export { tokenize } from "jsr:@vcltk/tokenizer@0.2.0";
-export type * from "../ast/mod.ts";
