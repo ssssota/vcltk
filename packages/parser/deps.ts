2b370a1be381a1bf8deb6844c42adc533e2ab687
TOMIKAWA Sotaro <sotaro.tomikawa@zozo.com>
2024-04-11 15:51:04 +0900

    fix(parser): set deps versions

--- packages/parser/deps.ts
+++ packages/parser/deps.ts
@@ -1,3 +1,3 @@
-export type * from "jsr:@vcltk/ast";
-export type * from "jsr:@vcltk/token";
-export { tokenize } from "jsr:@vcltk/tokenizer";
+export type * from "jsr:@vcltk/ast@0.1.1";
+export type * from "jsr:@vcltk/token@0.2.0";
+export { tokenize } from "jsr:@vcltk/tokenizer@0.2.0";
